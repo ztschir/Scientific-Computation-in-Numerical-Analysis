181a0cc636de08269a3b5169f74f3ee393194df8
Zachary Tschirhart <ztschirhart@tracking-point.com>
2014-02-07 13:47:00 -0600

    Added Secant method

---
+++ Homework2/secant.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% Secant Method %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [root, numIters] = secant(f,p0,p1,TOL,MaxIter)
+
+numIters = 1;
+q0 = f(p0);
+q1 = f(p1);
+if (p1 == 0) fprintf('p1 can not be zero'); end
+  
+while (numIters < MaxIter)
+  root = p1 - q1*((p1 - p0)/(q1 - p0));
+  
+  if (abs(p1 - p0)/abs(p0) < TOL)
+    return;
+  end
+
+  numIters = numIters + 1;
+  p0 = p1;
+  q0 = q1;
+  p1 = root;
+  q1 = f(root);
+  
+end
+
+fprintf('Method failed after %i iterations\n',numIters)
+
+end
